52d3086f5c70de0ccdd8440bec3ba527fc2e700b
fortkle <fortkle@gmail.com>
2016-01-13 06:19:31 +0900

    test9

--- phpcs_check_travis.sh
+++ phpcs_check_travis.sh
@@ -3,19 +3,15 @@
 set -eu
 
 echo "********************"
-echo "* install gems     *"
-echo "********************"
-gem install --no-document checkstyle_filter-git saddler saddler-reporter-github
-
-echo "********************"
 echo "* exec check       *"
 echo "********************"
 set +e
-git diff --name-only --diff-filter=ACMR origin/master...HEAD | grep ".php" | xargs vendor/bin/phpcs --standard=./phpcs.xml --report=checkstyle --report-file=phpcs.result.xml
+vendor/bin/phpcs ./app --report=checkstyle --report-file=phpcs.result.xml --standard=./phpcs.xml
 set -e
 
 echo "********************"
 echo "* checkstyle       *"
 echo "********************"
-cat phpcs.result.xml | saddler report --require saddler/reporter/github --reporter Saddler::Reporter::Github::PullRequestReviewComment
+cat phpcs.result.xml | checkstyle_filter-git diff origin/master | saddler report --require saddler/reporter/github --reporter Saddler::Reporter::Github::PullRequestReviewComment
+cat phpcs.result.xml | checkstyle_filter-git diff origin/master | saddler report --require saddler/reporter/github --reporter Saddler::Reporter::Github::CommitReviewComment
 
